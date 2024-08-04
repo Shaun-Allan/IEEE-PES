b9c238d7fb9c7598233289a32466edb86da0c116
Shaun-Allan <shaunallan0605@gmail.com>
2024-08-04 14:23:06 +0530

    h

---
+++ src/components/ElecContainer/ElecFlicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ElecFlicker.css';
+
+const ElecFlicker = ({ children }) => {
+  return (
+    <div className="elec-flicker-box">
+      {children}
+    </div>
+  );
+};
+
+export default ElecFlicker;
